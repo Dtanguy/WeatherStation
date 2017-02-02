0df92eb88f07b50fbd26694e4f7162567b5dcfc5
Tanguy <demme.tanguy@hotmail.fr>
2017-02-02 14:46:00 +0100

    update

--- nodeMCUsendData/nodeMCUsendData.ino
+++ nodeMCUsendData/nodeMCUsendData.ino
@@ -3,10 +3,10 @@ extern "C" {
   #include "user_interface.h"
 }
 
-const char* ssid = "";
+const char* ssid = ""
 const char* password = "";
-const char* host = "192.168.1.9";
- 
+const char* host = "";
+
 int nb_try = 0;
 
 void Dodo(int sec){
@@ -33,57 +33,54 @@ void setup() {
   WiFi.begin(ssid, password);
   
   nb_try = 0;
-  while (WiFi.status() != WL_CONNECTED) {
-    if (nb_try > 10){
-       nb_try = 0;
-       Dodo(60*10);
-    }
+  while (WiFi.status() != WL_CONNECTED && nb_try < 10) {  
     delay(500);
     Serial.print(".");
     nb_try++;
   }
 
-  Serial.println("");
-  Serial.println("WiFi connected");  
-  Serial.print("IP address: ");
-  Serial.println(WiFi.localIP());
-  Serial.print("forece : ");
-  Serial.println(WiFi.RSSI());
-
-  Serial.print("connecting to ");
-  Serial.println(host);
-  
-  // Use WiFiClient class to create TCP connections
-  WiFiClient client;
-  const int httpPort = 8085;
-  if (!client.connect(host, httpPort)) {
-    Serial.println("connection failed");
-    return;
+  if  (nb_try < 10){
+      Serial.println("");
+      Serial.println("WiFi connected");  
+      Serial.print("IP address: ");
+      Serial.println(WiFi.localIP());
+      Serial.print("forece : ");
+      Serial.println(WiFi.RSSI());
+    
+      Serial.print("connecting to ");
+      Serial.println(host);
+      
+      // Use WiFiClient class to create TCP connections
+      WiFiClient client;
+      const int httpPort = 8085;
+      if (!client.connect(host, httpPort)) {
+        Serial.println("connection failed");
+        return;
+      }
+    
+      String url = "/update?temp=";
+      url += analogRead(A0);
+      url += "&wifi=";
+      url += WiFi.RSSI();
+      
+      Serial.print("Requesting URL: ");
+      Serial.println(url);
+      
+      // This will send the request to the server
+      client.print(String("GET ") + url + " HTTP/1.1\r\n" + "Host: " + host + "\r\n" + "Connection: close\r\n\r\n");
+      delay(10);  
+      
+      // Read all the lines of the reply from server and print them to Serial
+      /*while(client.available()){
+        String line = client.readStringUntil('\r');
+        Serial.print(line);
+      }*/
   }
-
-  String url = "/update?temp=";
-  url += random(10, 30);
-  url += "&wifi=";
-  url += WiFi.RSSI();
-  
-  Serial.print("Requesting URL: ");
-  Serial.println(url);
-  
-  // This will send the request to the server
-  client.print(String("GET ") + url + " HTTP/1.1\r\n" + "Host: " + host + "\r\n" + "Connection: close\r\n\r\n");
-  delay(10);  
-  
-  // Read all the lines of the reply from server and print them to Serial
-  /*while(client.available()){
-    String line = client.readStringUntil('\r');
-    Serial.print(line);
-  }*/
-  
-  Dodo(60*10);  
 }
 
 
 void loop() {   
+    Dodo(60*10);  
 }
 
 
